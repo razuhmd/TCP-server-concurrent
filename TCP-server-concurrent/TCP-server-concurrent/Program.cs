863d6af5bbe64791d43327f408f3a5b17bf6b1c2
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-10-10 16:57:58 +0200

    Assignment 3

--- TCP-server-concurrent/TCP-server-concurrent/Program.cs
+++ TCP-server-concurrent/TCP-server-concurrent/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleRestService.Managers;
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -12,9 +13,10 @@ namespace TCP_server_concurrent
         {
             Console.WriteLine();
             Console.WriteLine("::::::::::::::::::::::::::::::SERVER::::::::::::::::::::::");
+            Console.WriteLine();
             //TcpListener listener = new TcpListener(System.Net.IPAddress.Loopback, 7);
             //TcpListener listener = new TcpListener(IPAddress.Parse("192.168.104.150"), 7);
-            TcpListener listener = new TcpListener(IPAddress.Any, 7);
+            TcpListener listener = new TcpListener(IPAddress.Any, 4646);
             listener.Start();
             
             while (true)
@@ -24,24 +26,45 @@ namespace TCP_server_concurrent
                 Task.Run(() => { HandleClient(socket); });
             }
 
-
         }
+
+
 
         public static void HandleClient(TcpClient socket)
         {
             NetworkStream ns = socket.GetStream();
             StreamReader reader = new StreamReader(ns);
             StreamWriter writer = new StreamWriter(ns);
+            BooksManager manager = new BooksManager();
 
             while (true)
             {
                 string message = reader.ReadLine();
 
-                if (message == "Exit")
-                { break; }
+                if (message == "Exit") { break; }
 
                 Console.WriteLine("Client Wrote : " + message);
-                writer.WriteLine(message.ToUpper());
+               // writer.WriteLine(message);
+                switch (message)
+                    {
+                    case "GetAll":
+                        var bookskList = manager.GetAll();
+                        foreach(var book in bookskList)
+                        {
+                            writer.WriteLine();
+                            writer.WriteLine(book.Title);
+                            writer.WriteLine(book.Author);
+                            writer.WriteLine(book.PageNumber);
+                            writer.WriteLine(book.ISBN13);
+                        }
+                        
+                        break;
+
+                    case "Get":
+                        writer.WriteLine("Insert ISBN: ");
+                        break;
+                    }
+                
                 writer.Flush();
             }
 
